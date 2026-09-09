1cfe81721391fecbb6e71be15d764ea31a4892ba
mrtada50 <mrtada1212@gmail.com>
2026-09-09 05:55:43 +0300

    Simplify settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,3 @@
-import org.gradle.api.initialization.resolve.RepositoriesMode
-
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 rootProject.name = "ProxyBrowser"
 include(":app")
+
